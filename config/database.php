6a0bbe9b3369680b4a9ad58e9587e161f06bc70f
Yousif Ayman <208421445+yousif-ayman-dev@users.noreply.github.com>
2026-08-27 13:52:56 -0700

    feat: auto-select mysql connection when railway mysql variables are detected

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', env('MYSQLHOST') || env('MYSQL_URL') || env('DATABASE_URL') ? 'mysql' : 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
